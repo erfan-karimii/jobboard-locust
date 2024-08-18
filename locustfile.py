d307a368deaa4252b299676eb0ceb2c8804feb58
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2024-08-18 10:30:02 +0330

    add a task

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,9 @@
-from faker import Faker
 import json
-
+import requests
+from random import randint
+from faker import Faker
 from locust import  TaskSet, task,FastHttpUser,constant
-
+import os
 
 fake = Faker()
 
@@ -21,6 +22,44 @@ class UserBehavior(TaskSet):
             response.raise_for_status()  # Raises an error for 4xx/5xx status codes
         except Exception as e:
             print(f"Request failed: {e}")
+    
+    @task
+    def update_profile_loadtest(self):
+        random_id = 2
+        payload = {
+            'id': random_id
+        }
+        headers = {'content-type': 'application/json'}
+        try:
+            x = requests.post("http://127.0.0.1:8000/load_test/p_update/", data=json.dumps(payload), headers=headers)
+            access = x.json().get("access")
+            if "notfound"  == access:
+                print("user not found")
+            else:
+                headers = {'content-type': 'multipart/form-data','Authorization':f'Bearer {access}'}
+                
+                
+                folder_path = './images'
+                files = os.listdir(folder_path)
+                y = randint(0,len(files)-1)
+                random_image = files[y]                
+                resume_file = open(folder_path + '/' + random_image, 'rb')  
+                file_path = folder_path + '/' + random_image
+                data = {
+                    "fullname" : str(fake.name()),
+                }
+                with open(file_path, 'rb') as image_file:
+                    files = {
+                        'resume_file': (os.path.basename(file_path), image_file, 'image/jpeg')
+                    }
+                
+                    response = self.client.post("/profile/", data=data,files=files, headers=headers)
+                    print(response.text)
+                    response.raise_for_status()
+                
+        except Exception as e:
+            print(f"Request failed: {e}")
+        
 
 
 class WebsiteUser(FastHttpUser):
