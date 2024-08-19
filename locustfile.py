4363c67adde3df488db6c048a163afd69ec2a013
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-08-19 14:54:00 +0330

    add get profile method

--- locustfile.py
+++ locustfile.py
@@ -7,6 +7,33 @@ import os
 
 fake = Faker()
 
+def login_user():
+    """
+    this function send POST for login end point and recive jwt (access Token) 
+    """
+    random_id = randint(1,10)
+    payload = {
+        'id': random_id
+    }
+    print(random_id)
+    headers = {'content-type': 'application/json'}
+    try:
+        x = requests.post("http://127.0.0.1:8000/load_test/p_update/", data=json.dumps(payload), headers=headers)
+        access = x.json().get("access")
+        if "notfound"  == access:
+            print("user not found")
+        else:
+            headers = {'content-type': 'multipart/form-data','Authorization':f'Bearer {access}'}
+            
+            return headers
+ 
+    except Exception as e:
+        print(f"Request failed: {e}")
+
+
+
+
+
 class UserBehavior(TaskSet):
     @task(1)
     def my_task(self):
@@ -23,43 +50,38 @@ class UserBehavior(TaskSet):
         except Exception as e:
             print(f"Request failed: {e}")
     
-    @task(6)
+    @task(4)
     def update_profile_loadtest(self):
-        random_id = randint(1,680000)
-        payload = {
-            'id': random_id
+        headers = login_user()
+        folder_path = './images'
+        files = os.listdir(folder_path)
+        y = randint(0,len(files)-1)
+        random_image = files[y]                
+        resume_file = open(folder_path + '/' + random_image, 'rb')  
+        file_path = folder_path + '/' + random_image
+        data = {
+            "fullname" : str(fake.name()),
         }
-        print(random_id)
-        headers = {'content-type': 'application/json'}
-        try:
-            x = requests.post("http://192.168.1.79/load_test/p_update/", data=json.dumps(payload), headers=headers)
-            access = x.json().get("access")
-            if "notfound"  == access:
-                print("user not found")
-            else:
-                headers = {'content-type': 'multipart/form-data','Authorization':f'Bearer {access}'}
-                
-                
-                folder_path = './images'
-                files = os.listdir(folder_path)
-                y = randint(0,len(files)-1)
-                random_image = files[y]                
-                resume_file = open(folder_path + '/' + random_image, 'rb')  
-                file_path = folder_path + '/' + random_image
-                data = {
-                    "fullname" : str(fake.name()),
-                }
-                with open(file_path, 'rb') as image_file:
-                    files = {
-                        'resume_file': (os.path.basename(file_path), image_file, 'image/jpeg')
-                    }
-                
-                    response = self.client.post("/profile/", data=data,files=files, headers=headers)
-                    print(response.text)
-                    response.raise_for_status()
-                
-        except Exception as e:
-            print(f"Request failed: {e}")
+        with open(file_path, 'rb') as image_file:
+            files = {
+                'resume_file': (os.path.basename(file_path), image_file, 'image/jpeg')
+            }
+        
+            response = self.client.post("/profile/", data=data,files=files, headers=headers)
+            print(response.text)
+            response.raise_for_status()
+
+
+    @task(8)
+    def profile(self):
+        
+        """
+        get User Profile --> Customer
+        """
+        
+        headers = login_user()
+        x=self.client.get('/profile/', headers=headers)
+
         
 
 
