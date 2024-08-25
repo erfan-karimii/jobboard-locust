d2aad7ec3a49e08bd184a1ea7a6732b7aedcb985
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-08-25 18:29:05 +0330

    its OK

--- locustfile.py
+++ locustfile.py
@@ -69,65 +69,65 @@ def login_random_user():
 
 
 class UserBehavior(TaskSet):
-    # @task(1)
-    # def create_user_load_test(self):
-    #     email = str(fake.name()) + str(fake.city()) + str(fake.random_int())
-    #     email_without_space = "".join(email.split())
-    #     payload = {
-    #         "email": f"{email_without_space}@gmail.com",
-    #     }
-    #     headers = {"content-type": "application/json"}
-    #     try:
-    #         response = self.client.post(
-    #             "/account/user/login/", data=json.dumps(payload), headers=headers
-    #         )
-    #         print(response.text)
-    #         response.raise_for_status()  # Raises an error for 4xx/5xx status codes
-    #     except Exception as e:
-    #         print(f"Request failed: {e}")
+    @task(1)
+    def create_user_load_test(self):
+        email = str(fake.name()) + str(fake.city()) + str(fake.random_int())
+        email_without_space = "".join(email.split())
+        payload = {
+            "email": f"{email_without_space}@gmail.com",
+        }
+        headers = {"content-type": "application/json"}
+        try:
+            response = self.client.post(
+                "/account/user/login/", data=json.dumps(payload), headers=headers
+            )
+            print(response.text)
+            response.raise_for_status()  # Raises an error for 4xx/5xx status codes
+        except Exception as e:
+            print(f"Request failed: {e}")
 
 
-    # @task(4)
-    # def update_profile_loadtest(self):
-    #     headers = login_random_user()
-    #     folder_path = "./images"
+    @task(4)
+    def update_profile_loadtest(self):
+        headers = login_random_user()
+        folder_path = "./images"
         
-    #     # List files once and use choice for random selection
-    #     files = os.listdir(folder_path)
-    #     if not files:
-    #         print("No files found in the folder.")
-    #         return
+        # List files once and use choice for random selection
+        files = os.listdir(folder_path)
+        if not files:
+            print("No files found in the folder.")
+            return
         
-    #     random_image = choice(files)  # More efficient random selection
-    #     file_path = os.path.join(folder_path, random_image)  # Use os.path.join for better path handling
+        random_image = choice(files)  # More efficient random selection
+        file_path = os.path.join(folder_path, random_image)  # Use os.path.join for better path handling
         
-    #     data = {
-    #         "fullname": str(fake.name()),
-    #     }
+        data = {
+            "fullname": str(fake.name()),
+        }
         
-    #     try:
-    #         with open(file_path, "rb") as image_file:
-    #             response = self.client.patch(
-    #                 "/account/user/profile/", 
-    #                 data=data, 
-    #                 files={"resume_file": (os.path.basename(file_path), image_file, "image/jpeg")}, 
-    #                 headers=headers
-    #             )
-    #             print(response.text)
-    #             response.raise_for_status()
-    #     except OSError as e:
-    #         print(f"File operation failed: {e}")
-    #     except Exception as e:
-    #         print(f"An error occurred: {e}")
+        try:
+            with open(file_path, "rb") as image_file:
+                response = self.client.patch(
+                    "/account/user/profile/", 
+                    data=data, 
+                    files={"resume_file": (os.path.basename(file_path), image_file, "image/jpeg")}, 
+                    headers=headers
+                )
+                print(response.text)
+                response.raise_for_status()
+        except OSError as e:
+            print(f"File operation failed: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    # @task(8)
-    # def profile(self):
-    #     """
-    #     get User Profile --> Customer
-    #     """
+    @task(8)
+    def profile(self):
+        """
+        get User Profile --> Customer
+        """
 
-    #     headers = login_random_user()
-    #     response  = self.client.get("/account/user/profile/", headers=headers)
+        headers = login_random_user()
+        response  = self.client.get("/account/user/profile/", headers=headers)
 
 
     @task
@@ -144,11 +144,11 @@ class UserBehavior(TaskSet):
         avg = mean([min_salary,max_salary])
         
         info = csv_file.iloc[number]['Job Description'] + "\n" + csv_file.iloc[number]['skills'] + "\n" + csv_file.iloc[number]['Responsibilities']
-                
+        category = randint(1,20)  
         print(int(avg))
         data = {
             "title":csv_file.iloc[number]['Job Title'],
-            "category":1,
+            "category":category,
             "province":choice(PROVINCE_CHOICES),
             "salary": int(avg),
             "info":info,
